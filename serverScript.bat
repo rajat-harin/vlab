a6426c68390435722529ecfd4f7d6793090b33e1
rajat-harin <rajat.harinkhede@gmail.com>
2021-01-28 10:27:54 +0530

    .gitignore Fixed

--- serverScript.bat
+++
@@ -1,3 +0,0 @@
-echo "starting inside 1"
-npm run server
-echo "exiting inside 1"
--- startScript.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-Rem This is for staring node server
-cd "D:\Workspace\final year project\vlab"
-echo "directry changed"
-start "client script" "D:\Workspace\final year project\vlab\clientScript.bat"
-echo "client script started..."
-start "server script" "D:\Workspace\final year project\vlab\serverScript.bat"
-echo "server script started..."
-exit
